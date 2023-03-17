e0a16cadb0c29ce8e31535c64e8a11541e58a063
Luigi9411 <luigi.sciavicco@gmail.com>
2023-03-17 15:38:49 +0100

    final

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,72 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int[] Array = { 2, 6, 7, 5, 3, 9 };
+printArray(Array);
+int[] squaredArray = RaiseToSquare(Array);
+printArray(squaredArray);
+int somma = ArraySum(Array);
+Console.WriteLine("La somma totale degli elementi dell'array è: " + somma);
+int sumSquare = ArraySum(squaredArray);
+Console.WriteLine("La somma totale degli elementi dell'array è: " + sumSquare);
+
+
+void printArray(int[] array)
+{
+    Console.WriteLine(arrayToString(array));
+}
+
+string arrayToString(int[] array)
+{
+    var result = "[";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i].ToString();
+
+        if (i < array.Length - 1)
+        {
+            result += ", ";
+        }
+    }
+
+    result += "]";
+
+    return result;
+}
+
+//funzione per il quadrato
+
+int square(int number)
+{
+    int square = number * number;
+    return square;
+}
+
+int[] RaiseToSquare(int[] array)
+{
+    int[] squaredArray = (int[])array.Clone();
+
+    for (int i = 0; i < squaredArray.Length; i++)
+    {
+        squaredArray[i] = squaredArray[i] * squaredArray[i];
+    }
+
+    return squaredArray;
+}
+
+//funzione somma
+
+int ArraySum(int[] array)
+{
+    int somma = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        somma += array[i];
+    }
+
+    return somma;
+}
+
+
+
